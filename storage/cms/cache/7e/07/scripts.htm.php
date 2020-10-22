f99f9b76747574f4ee57b441e22ac9ddac5c006c
nonstop <git@eve.si>
2020-10-22 16:01:28 +0200

    updated site with 1 new image on 2 pages

--- storage/cms/cache/7e/07/scripts.htm.php
+++ storage/cms/cache/7e/07/scripts.htm.php
@@ -1,5 +1,5 @@
 <?php 
-class Cms5f7edaf95b0a5245558290_cf7726cc113650a0d1899afa69c2e058Class extends Cms\Classes\PartialCode
+class Cms5f8fef77de3e5237964564_565715b9b0946cda80b96f310d7768a9Class extends Cms\Classes\PartialCode
 {
 
 }
--- storage/cms/twig/77/772a3b1c0e59fe7e312f5eaedadd26a8c4e23082a80fc1eb23ba808175cff001.php
+++ storage/cms/twig/77/772a3b1c0e59fe7e312f5eaedadd26a8c4e23082a80fc1eb23ba808175cff001.php
@@ -63,12 +63,11 @@ class __TwigTemplate_3298e10ab2af222b6ef98fad9f6c4c5b5d3d6adb8474c4e97984fe54bc3
         echo $this->extensions['Cms\Twig\Extension']->themeFilter([0 => "assets/vendor/jquery/dist/jquery.min.js", 1 => "assets/vendor/tether/dist/js/tether.min.js", 2 => "assets/vendor/bootstrap/dist/js/bootstrap.min.js", 3 => "assets/vendor/wow/dist/wow.min.js", 4 => "assets/vendor/holderjs/holder.min.js", 5 => "assets/vendor/plyr/dist/plyr.js", 6 => "assets/vendor/lazyload/dist/lazyload.min.js", 7 => "assets/js/scripts.js"]);
         // line 10
         echo "\"></script>
-    ";
+";
         // line 11
         echo $this->env->getExtension('Cms\Twig\Extension')->assetsFunction('js');
         echo $this->env->getExtension('Cms\Twig\Extension')->displayBlock('scripts');
         // line 12
-        echo "    ";
         $_minify = System\Classes\CombineAssets::instance()->useMinify;
         if ($_minify) {
             echo '<script src="' . Request::getBasePath() . '/modules/system/assets/js/framework.combined-min.js"></script>'.PHP_EOL;
@@ -80,13 +79,22 @@ class __TwigTemplate_3298e10ab2af222b6ef98fad9f6c4c5b5d3d6adb8474c4e97984fe54bc3
         echo '<link rel="stylesheet" property="stylesheet" href="' . Request::getBasePath() .'/modules/system/assets/css/framework.extras'.($_minify ? '-min' : '').'.css">'.PHP_EOL;
         unset($_minify);
         // line 13
-        echo "    <script>
-      \$(document).ready(function () {
+        echo "<script>
+  \$(document).ready(function () {
 
-        //lazyload();
+    //lazyload();
 
-      });      
-    </script>";
+  });      
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-98455217-8\"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-98455217-8');
+</script>";
     }
 
     public function getTemplateName()
@@ -101,29 +109,38 @@ class __TwigTemplate_3298e10ab2af222b6ef98fad9f6c4c5b5d3d6adb8474c4e97984fe54bc3
 
     public function getDebugInfo()
     {
-        return array (  83 => 13,  71 => 12,  68 => 11,  65 => 10,  62 => 1,);
+        return array (  82 => 13,  71 => 12,  68 => 11,  65 => 10,  62 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Source("<script src=\"{{ [
-      'assets/vendor/jquery/dist/jquery.min.js',
-      'assets/vendor/tether/dist/js/tether.min.js',
-      'assets/vendor/bootstrap/dist/js/bootstrap.min.js',
-      'assets/vendor/wow/dist/wow.min.js',
-      'assets/vendor/holderjs/holder.min.js',
-      'assets/vendor/plyr/dist/plyr.js',
-      'assets/vendor/lazyload/dist/lazyload.min.js',
-      'assets/js/scripts.js'
-    ]|theme }}\"></script>
-    {% scripts %}
-    {% framework extras %}
-    <script>
-      \$(document).ready(function () {
+  'assets/vendor/jquery/dist/jquery.min.js',
+  'assets/vendor/tether/dist/js/tether.min.js',
+  'assets/vendor/bootstrap/dist/js/bootstrap.min.js',
+  'assets/vendor/wow/dist/wow.min.js',
+  'assets/vendor/holderjs/holder.min.js',
+  'assets/vendor/plyr/dist/plyr.js',
+  'assets/vendor/lazyload/dist/lazyload.min.js',
+  'assets/js/scripts.js'
+]|theme }}\"></script>
+{% scripts %}
+{% framework extras %}
+<script>
+  \$(document).ready(function () {
 
-        //lazyload();
+    //lazyload();
 
-      });      
-    </script>", "/home/vagrant/code/prova/themes/prova/partials/scripts.htm", "");
+  });      
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-98455217-8\"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-98455217-8');
+</script>", "/home/vagrant/code/prova/themes/prova/partials/scripts.htm", "");
     }
 }
